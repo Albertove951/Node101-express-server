94b1ad640158e516f24e008e8211471973a7e42d
Alberto Vega <albertovega00@gmail.com>
2020-01-11 16:40:04 -0800

    first commit

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 // import files and packages up here
-
+var express = require('express')
+var morgan = require('morgan')
+var data = require('./data')
 
 // create your express server below
-var app;
+var app = express()
 
 // add your routes and middleware below
+    // app.use(morgan('combined'))
+    app.get('/', function (req, res) {
+        res.status(200).send('we made it')
+    })
+    
+    app.get('/data', function (req, res) {
+        res.send(data)
+    })
 
 // finally export the express application
 module.exports = app;
